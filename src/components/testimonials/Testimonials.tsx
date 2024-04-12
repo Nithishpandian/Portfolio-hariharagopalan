18528aa5f685faffbe18e4ac5f5b747e646d4166
Nithishpandian <nithishpandiyan75@gmail.com>
2024-04-12 19:56:04 +0530

    added gradient

--- src/components/testimonials/Testimonials.tsx
+++ src/components/testimonials/Testimonials.tsx
@@ -4,8 +4,9 @@ import indiaFlag from "../../assets/india-flag.png";
 
 const Testimonials = () => {
   return (
-    <div className=" grid grid-cols-4 grid-rows-2 gap-4 items-center justify-between px-10 pt-20 pb-24">
-      <div className=" flex flex-col gap-5 col-span-2 bg-gradient-to-tr from-stone-900 to-stone-800 shadow-lg border border-stone-700 backdrop-filter backdrop-blur-lg bg-opacity-30 px-5 py-5 rounded-md h-full w-full cursor-pointer">
+    <div className=" grid grid-cols-4 grid-rows-2 gap-4 items-center justify-between px-10 pt-20 pb-24 relative">
+      <div className="absolute w-full h-[500px] bg-gradient-radial blur-3xl z-0 from-[#050000] right-0 top-[20%]"></div>
+      <div className=" group hover:border-stone-500 hover:rounded duration-500 flex flex-col gap-5 col-span-2 bg-gradient-to-tr from-stone-900 to-stone-800 shadow-lg border border-stone-700 backdrop-filter backdrop-blur-lg bg-opacity-30 px-5 py-5 rounded-md h-full w-full cursor-pointer">
         <div className=" flex items-center gap-3">
           <Image
             src={indiaFlag}
@@ -15,11 +16,13 @@ const Testimonials = () => {
             alt="Flag"
           />
           <div>
-            <h1 className=" text-stone-200 font-bold text-lg">John doe</h1>
+            <h1 className=" group-hover:text-stone-100 duration-500 text-stone-200 font-bold text-lg">
+              John doe
+            </h1>
             <p className=" text-stone-300 text-sm font-medium">Professor</p>
           </div>
         </div>
-        <p className=" text-stone-200 font-medium">
+        <p className="group-hover:text-stone-100 duration-500 text-stone-200 font-medium">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere enim a
           quo, labore autem illum incidunt. Maiores repellendus delectus ad
           necessitatibus eveniet nam ducimus reprehenderit id quos molestias.
@@ -28,7 +31,7 @@ const Testimonials = () => {
           reprehenderit perferendis, voluptatem asperiores voluptas tenetur.
         </p>
       </div>
-      <div className=" flex flex-col gap-5 bg-gradient-to-t from-neutral-900 to-neutral-800 shadow-lg border border-stone-700 backdrop-filter backdrop-blur-lg bg-opacity-30 px-5 py-5 rounded-md h-full w-full cursor-pointer">
+      <div className=" group hover:border-stone-500 hover:rounded duration-500 flex flex-col gap-5 bg-gradient-to-t from-neutral-900 to-neutral-800 shadow-lg border border-stone-700 backdrop-filter backdrop-blur-lg bg-opacity-30 px-5 py-5 rounded-md h-full w-full cursor-pointer">
         <div className=" flex items-center gap-3">
           <Image
             src={indiaFlag}
@@ -38,17 +41,19 @@ const Testimonials = () => {
             alt="Flag"
           />
           <div>
-            <h1 className=" text-stone-200 font-bold text-lg">John doe</h1>
+            <h1 className=" group-hover:text-stone-100 duration-500 text-stone-200 font-bold text-lg">
+              John doe
+            </h1>
             <p className=" text-stone-300 text-sm font-medium">Professor</p>
           </div>
         </div>
-        <p className=" text-stone-200 font-medium">
+        <p className=" group-hover:text-stone-100 duration-500 text-stone-200 font-medium">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores optio
           corrupti eum velit explicabo possimus error rerum repellendus
           consequatur nulla!
         </p>
       </div>
-      <div className=" flex flex-col gap-5 row-span-2 bg-gradient-to-l from-zinc-900 to-zinc-800 shadow-lg border border-stone-700 backdrop-filter backdrop-blur-lg bg-opacity-30 px-5 py-5 rounded-md h-full w-full cursor-pointer">
+      <div className=" group hover:border-stone-500 hover:rounded duration-500 flex flex-col gap-5 row-span-2 bg-gradient-to-l from-zinc-900 to-zinc-800 shadow-lg border border-stone-700 backdrop-filter backdrop-blur-lg bg-opacity-30 px-5 py-5 rounded-md h-full w-full cursor-pointer">
         <div className=" flex items-center gap-3">
           <Image
             src={indiaFlag}
@@ -58,11 +63,13 @@ const Testimonials = () => {
             alt="Flag"
           />
           <div>
-            <h1 className=" text-stone-200 font-bold text-lg">John doe</h1>
+            <h1 className=" group-hover:text-stone-100 duration-500 text-stone-200 font-bold text-lg">
+              John doe
+            </h1>
             <p className=" text-stone-300 text-sm font-medium">Professor</p>
           </div>
         </div>
-        <p className=" text-stone-200 font-medium">
+        <p className=" group-hover:text-stone-100 duration-500 text-stone-200 font-medium">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia
           magnam quaerat amet eius laudantium officia soluta sit, autem
           reprehenderit. Omnis, aspernatur. Nihil dolor ratione commodi aperiam?
@@ -71,7 +78,7 @@ const Testimonials = () => {
           molestiae pariatur laborum. Minima, quod temporibus!
         </p>
       </div>
-      <div className=" flex flex-col gap-5 bg-gradient-to-tr from-stone-900 to-zinc-800 shadow-lg border border-stone-700 backdrop-filter backdrop-blur-lg bg-opacity-30 px-5 py-5 rounded-md h-full w-full cursor-pointer">
+      <div className=" group hover:border-stone-500 hover:rounded duration-500 flex flex-col gap-5 bg-gradient-to-tr from-stone-900 to-zinc-800 shadow-lg border border-stone-700 backdrop-filter backdrop-blur-lg bg-opacity-30 px-5 py-5 rounded-md h-full w-full cursor-pointer">
         <div className=" flex items-center gap-3">
           <Image
             src={indiaFlag}
@@ -81,17 +88,19 @@ const Testimonials = () => {
             alt="Flag"
           />
           <div>
-            <h1 className=" text-stone-200 font-bold text-lg">John doe</h1>
+            <h1 className=" group-hover:text-stone-100 duration-500 text-stone-200 font-bold text-lg">
+              John doe
+            </h1>
             <p className=" text-stone-300 text-sm font-medium">Professor</p>
           </div>
         </div>
-        <p className=" text-stone-200 font-medium">
+        <p className=" group-hover:text-stone-100 duration-500 text-stone-200 font-medium">
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius ea
           asperiores quis accusamus necessitatibus ducimus voluptatem! Iste
           eligendi aliquam odio.
         </p>
       </div>
-      <div className=" flex flex-col gap-5 col-span-2 bg-gradient-to-tl from-stone-900 via-stone-800 to-zinc-800 shadow-lg border border-stone-700 backdrop-filter backdrop-blur-lg bg-opacity-30 px-5 py-5 rounded-md h-full w-full cursor-pointer">
+      <div className=" group hover:border-stone-500 hover:rounded duration-500 flex flex-col gap-5 col-span-2 bg-gradient-to-tl from-stone-900 via-stone-800 to-zinc-800 shadow-lg border border-stone-700 backdrop-filter backdrop-blur-lg bg-opacity-30 px-5 py-5 rounded-md h-full w-full cursor-pointer">
         <div className=" flex items-center gap-3">
           <Image
             src={indiaFlag}
@@ -101,11 +110,13 @@ const Testimonials = () => {
             alt="Flag"
           />
           <div>
-            <h1 className=" text-stone-200 font-bold text-lg">John doe</h1>
+            <h1 className=" group-hover:text-stone-100 duration-500 text-stone-200 font-bold text-lg">
+              John doe
+            </h1>
             <p className=" text-stone-300 text-sm font-medium">Professor</p>
           </div>
         </div>
-        <p className=" text-stone-200 font-medium">
+        <p className=" group-hover:text-stone-100 duration-500 text-stone-200 font-medium">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima
           corrupti sint asperiores, nam, cum soluta, in sapiente ipsam totam
           quia aspernatur vel! Modi in id a cupiditate praesentium. Doloribus
